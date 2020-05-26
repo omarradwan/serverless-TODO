2aa4cd10c8f651994553086264dd75130ff78dd1
Omar Radwan <omarradwan213@gmail.com>
2020-05-26 19:09:51 +0200

    refactor: todo dao

--- backend/src/data/todoDao.ts
+++ backend/src/data/todoDao.ts
@@ -2,28 +2,27 @@
 import * as AWS  from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-
-const XAWS = AWSXRay.captureAWS(AWS)
-
 import { TodoItem } from '../models/TodoItem'
 import { createLogger } from '../utils/logger'
 import { TodoUpdate } from '../models/TodoUpdate'
 
 const logger = createLogger('auth')
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const todosTableName = process.env.TODOS_TABLE
+const todoIdIndex = process.env.TODO_ID_INDEX
 
 export class TodoDao {
-
+    
     constructor(
-        private readonly docClient: DocumentClient = createDynamoDBClient(),
-        private readonly TodosTableName = process.env.TODOS_TABLE,
-        private readonly TodoIdIndex = process.env.TODO_ID_INDEX) {
+        private readonly docClient: DocumentClient = createDynamoDBClient()) {
     }
 
     async getTodosOfUser(userId: string): Promise<TodoItem[]> {
         logger.info(`Getting all todos for user with id: ${userId}`)
     
         const result = await this.docClient.query({
-          TableName: this.TodosTableName,
+          TableName: todosTableName,
           KeyConditionExpression: 'userId = :userId',
           ExpressionAttributeValues: {
               ':userId': userId
@@ -33,55 +32,47 @@ export class TodoDao {
     
         const items = result.Items
         return items as TodoItem[]
-      }
+    }
 
-    async createTODO(todoItem: TodoItem): Promise<TodoItem>{
-        logger.info(`Creating new todo item for user with id: ${todoItem.userId}`)
+    async createTodo(todoItem: TodoItem): Promise<TodoItem>{
+        logger.info(`Creating new todo item: ${todoItem}`)
 
         await this.docClient.put({
-            TableName: this.TodosTableName,
+            TableName: todosTableName,
             Item: todoItem
           }).promise()
       
           return todoItem
     }
 
-    async updateTODO(todoId: string, todoUpdate: TodoUpdate): Promise<TodoUpdate>{
+    async updateTodo(todoId: string, todoUpdate: TodoUpdate): Promise<void>{
         logger.info(`Updating existing todo item with id: ${todoId}`)
 
-        const todoItem = await this.getTodoById(todoId)
+        const oldItem = await this.getTodoById(todoId)
 
-        await this.docClient.update({
-            TableName: this.TodosTableName,
-            Key: { userId: todoItem.userId, createdAt: todoItem.createdAt },
-            UpdateExpression: 'set name = :name, dueDate = :dueDate, done = :done',
-            ExpressionAttributeValues: {
-                ':name': todoUpdate.name,
-                ':dueDate': todoUpdate.dueDate,
-                ':done': todoUpdate.done
-            }
-          }).promise()
-      
-          return todoUpdate
+        await this.docClient.put({
+            TableName: todosTableName,
+            Item: this.getUpdatedTodoItem(oldItem, todoUpdate)
+          }).promise()    
     }
 
-    async deleteTODO(todoId: string): Promise<void>{
+    async deleteTodo(todoId: string): Promise<void>{
         logger.info(`Deleting existing todo item with id: ${todoId}`)
         
         const todoItem = await this.getTodoById(todoId)
 
         await this.docClient.delete({
-            TableName: this.TodosTableName,
+            TableName: todosTableName,
             Key: { userId: todoItem.userId, createdAt: todoItem.createdAt },
           }).promise()      
     }
 
-    async getTodoById(id: string): Promise<TodoItem> {
+    private async getTodoById(id: string): Promise<TodoItem> {
         logger.info(`Getting todo item with id: ${id}`)
 
         const result = await this.docClient.query({
-            TableName: this.TodosTableName,
-            IndexName: this.TodoIdIndex,
+            TableName: todosTableName,
+            IndexName: todoIdIndex,
             KeyConditionExpression: 'todoId = :id',
             ExpressionAttributeValues: {
                 ':id': id
@@ -94,7 +85,24 @@ export class TodoDao {
           }
           const items = result.Items
           return items[0] as TodoItem
-      }
+    }
+
+    private getUpdatedTodoItem(oldItem: TodoItem, updates: TodoUpdate): object {
+        logger.info(`updating todo: ${oldItem} with the following values: ${updates}`)
+
+        let newItem = {}
+        for (let key in updates) {
+            if (updates[key]) {
+                newItem[key] = updates[key]
+            } else {
+                newItem[key] = oldItem[key]
+            }
+        }
+
+        logger.info(`updated todo is: ${newItem}`)
+
+        return newItem
+    }
 }
 
 function createDynamoDBClient() {
