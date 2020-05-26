fe8940a74732053a7de92ded277174340914e6ce
Omar Radwan <omarradwan213@gmail.com>
2020-05-27 00:21:52 +0200

    chore: Add deployed apiKey

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'k10poi4xt3'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'dev-7mvxnk67.eu.auth0.com',
